85a2f5673866173229e036b0f7bfa1e3cff89586
marmottatonante <tommaso.detommaso@protonmail.com>
2026-04-05 21:00:20 +0200

    Added IBindable documentation

--- Reactivity/IBindable.cs
+++ Reactivity/IBindable.cs
@@ -1,5 +1,9 @@
 ﻿namespace Keystone.Reactivity;
 
+/// <summary>
+/// Represents an object capable of being bound.
+/// </summary>
+/// <typeparam name="T">Type of the binded value.</typeparam>
 public interface IBindable<T>
 {
     void Bind(Func<T> compute, params IWatchable[] sources);
